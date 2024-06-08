1244939ab8d57cd0e21d899034664e5bdb989233
dns-lector <157026463+dns-lector@users.noreply.github.com>
2024-06-08 17:43:09 +0300

    Initial commit

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;   // Посилання на компонент того ж ГО, на якому скрипт
+
+    void Start()
+    {
+        Debug.Log("BirdScript Start");
+        // пошук компонента та одержання посилання на нього
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidBody.AddForce(new Vector2(0, 500));
+        }
+    }
+}
